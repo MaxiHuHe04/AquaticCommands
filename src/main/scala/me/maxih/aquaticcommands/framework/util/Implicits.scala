6804cc8ba51ebfedb41d83c4c1f1dba2e0e4bb29
MaxiHuHe04 <maxiherczegh@outlook.com>
2018-09-01 18:28:07 +0200

    Added implicits for location senders

--- src/main/scala/me/maxih/aquaticcommands/framework/util/Implicits.scala
+++ src/main/scala/me/maxih/aquaticcommands/framework/util/Implicits.scala
@@ -1,8 +1,9 @@
 package me.maxih.aquaticcommands.framework.util
 
 import me.maxih.aquaticcommands.framework.CommandException
+import org.bukkit.Location
 import org.bukkit.command.{BlockCommandSender, CommandSender}
-import org.bukkit.entity.Player
+import org.bukkit.entity.{Entity, Player}
 
 /**
   * Created by Maxi H. on 11.08.2018
@@ -17,12 +18,21 @@ object Implicits {
 
     def playerOrNull: Player = player.orNull
 
-    def playerOrError: Player = player.getOrElse(throw CommandException("A player is required for this command."))
+    def playerOrError: Player = player.getOrElse(throw CommandException("A player is required for this command"))
 
     def blockSender: Option[BlockCommandSender] = self match {
       case self: BlockCommandSender => Some(self)
       case _ => None
     }
+
+    def location: Option[Location] = self match {
+      case self: Player => Some(self.getLocation)
+      case self: Entity => Some(self.getLocation)
+      case self: BlockCommandSender => Some(self.getBlock.getLocation)
+      case _ => None
+    }
+
+    def locationOrError: Location = location.getOrElse(throw CommandException("A sender with location is required for this command"))
   }
 
   implicit class RichOption[A](self: Option[A]) {
